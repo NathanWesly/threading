5776c6f1b764fbe7ba1e32ce25c01c8cff72eb44
Nathan O Wesly <nathan.o.wesly@gmail.com>
2015-03-11 08:32:26 +0300

    added comments, improved formatting

--- Thread.php
+++ Thread.php
@@ -7,31 +7,49 @@
  */ 
 class Thread
 {
+	/**
+	 * Init a thread
+	 */ 
 	public function __construct()
 	{
 		return "worker";
 	}
-
+    
+    /**
+     * Stops a worker thread
+     */ 
 	public function stop()
 	{
 		return "stopped";
 	}
-
+    
+    /**
+     * Starts a worker thread
+     */ 
 	public function start()
 	{
 		return "started";
 	}
-
+    
+    /**
+     * kills a worker thread 
+     */
 	public function kill()
 	{
 		return "killed";
 	}
-
+    
+    /**
+     * return worker handler
+     */
 	public function getWorkerHandler()
 	{
 		return 'Worker handler';
 	}
-
+    
+    /**
+     * kills a thread
+     */ 
 	public function __destruct()
 	{
 		$this->kill();
